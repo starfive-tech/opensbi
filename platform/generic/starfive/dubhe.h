065ef719373d3ed2f9761dd7cd137edd4b369e29
Chee Hong Ang <cheehong.ang@starfivetech.com>
2024-01-14 15:47:34 +0800

    platform: generic: starfive: Add Dubhe CMO SBI extension
    Add L2 cache flush and L2 cache invalidate SBI calls for Dubhe
    
    Signed-off-by: Chee Hong Ang <cheehong.ang@starfivetech.com>

---
+++ platform/generic/starfive/dubhe.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 StarFive Technology Co., Ltd.
+ *
+ * Author: Jun Liang Tan <junliang.tan@linux.starfivetech.com>
+ *
+ */
+
+#ifndef _STARFIVE_DUBHE_H_
+#define _STARFIVE_DUBHE_H_
+
+enum sbi_ext_dubhe_cmo_fid {
+	SBI_EXT_STARFIVE_DUBHE_L2_FLUSH = 0,
+	SBI_EXT_STARFIVE_DUBHE_L2_INVALIDATE,
+};
+
+#endif /* _STARFIVE_DUBHE_H_ */
