starfive-tech/opensbi

commit ab5ee80c1ea88f5e6a85b6a7864185d5a034692e

platform: generic: starfive: dubhe: Add Dubhe-90/80 hart PAUSE support
PAUSE instruction is a HINT that indicates the current hart’s rate of
instruction retirement should be temporarily reduced or paused.

Dubhe-90/80 predates ZIHINTPAUSE ISA. This patch adds StarFive vendor
specific implementation to support enabling of PAUSE instruction to
the hart in Dubhe-90/80 core.
This instruction causes a stall of up to 256 cycles or any external
interrupt is seen.

Signed-off-by: Ji Sheng Teoh <jisheng.teoh@starfivetech.com>

platform/generic/starfive/dubhe.c
@@ -10,9 +10,22 @@
 #include <cache.h>
 #include <dubhe.h>
 #include <platform_override.h>
+#include <sbi/riscv_asm.h>
 #include <sbi/sbi_console.h>
 #include <sbi/sbi_error.h>
 #include <sbi_utils/fdt/fdt_helper.h>
+
+static void starfive_dubhe_pause_enable(void)
+{
+	csr_write(CSR_HART_CTRL, CSR_HART_PAUSE_EN_MASK);
+}
+
+static int starfive_dubhe_extensions_init(const struct fdt_match *match,
+					   struct sbi_hart_features *hfeatures)
+{
+	starfive_dubhe_pause_enable();
+	return 0;
+}
 
 static int starfive_vendor_ext_provider(long funcid,
 					struct sbi_trap_regs *regs,
@@ -44,5 +57,6 @@ static const struct fdt_match starfive_dubhe_match[] = {
 
 const struct platform_override starfive_dubhe = {
 	.match_table = starfive_dubhe_match,
+	.extensions_init = starfive_dubhe_extensions_init,
 	.vendor_ext_provider = starfive_vendor_ext_provider,
 };

platform/generic/starfive/dubhe.h
@@ -10,6 +10,10 @@
 #ifndef _STARFIVE_DUBHE_H_
 #define _STARFIVE_DUBHE_H_
 
+#define CSR_HART_CTRL			0xbd0
+#define CSR_HART_PAUSE_EN_OFFSET	11
+#define CSR_HART_PAUSE_EN_MASK		BIT(CSR_HART_PAUSE_EN_OFFSET)
+
 enum sbi_ext_dubhe_cmo_fid {
 	SBI_EXT_STARFIVE_DUBHE_L2_FLUSH = 0,
 	SBI_EXT_STARFIVE_DUBHE_L2_INVALIDATE,

commit 1ec6c0507ab0073e0ef5d53f96db499d70154e9b

platform: generic: starfive: dubhe: Fix Dubhe-90/80 PAUSE time
ptime[7:0] field in CSR_HART_CTRL comes with a default hex value of 0xff.
Fix the code to not overwrite this value.

Fixes: ab5ee80c1ea8 ("platform: generic: starfive: dubhe: Add Dubhe-90/80 hart PAUSE support")
Signed-off-by: Ji Sheng Teoh <jisheng.teoh@starfivetech.com>

platform/generic/starfive/dubhe.c
@@ -17,7 +17,7 @@
 
 static void starfive_dubhe_pause_enable(void)
 {
-	csr_write(CSR_HART_CTRL, CSR_HART_PAUSE_EN_MASK);
+	csr_set(CSR_HART_CTRL, CSR_HART_PAUSE_EN_MASK);
 }
 
 static int starfive_dubhe_extensions_init(const struct fdt_match *match,